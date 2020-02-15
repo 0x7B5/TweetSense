bffab1e6f55f4ce8dbf6d66e2da3f85e2926d863
Vlad Munteanu <vladcmunteanu@gmail.com>
2020-02-15 16:37:39 -0500

    Added topics to api.

--- TwitterApi/main.py
+++ TwitterApi/main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 from flask import Flask, jsonify
-from twitterscraper import query_tweets_from_user
+from twitterscraper import query_tweets_from_user, query_tweets
 
 app = Flask(__name__, static_url_path="")
 @app.route('/tweets/<username>', methods=['GET'])
@@ -19,6 +19,19 @@ def getTweets(username):
 @app.route('/deepTweets/<username>', methods=['GET'])
 def getDeepTweets(username):
     list_of_tweets = query_tweets_from_user(username, None)
+    tweets = [ ]
+    #print the retrieved tweets to the screen:
+    for tweet in list_of_tweets:
+        tweets.append({
+                        "Date": tweet.timestamp,
+                        "Text": tweet.text
+                      })
+                      
+    return jsonify({'Data': tweets})
+
+@app.route('/topic/<topic>', methods=['GET'])
+def getTopicTweets(topic):
+    list_of_tweets = query_tweets(topic, 100)
     tweets = [ ]
     #print the retrieved tweets to the screen:
     for tweet in list_of_tweets:
