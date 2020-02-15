77b7af5fba2dd6aab3133771ffe1e006e98027db
Vlad Munteanu <vladcmunteanu@gmail.com>
2020-02-14 20:10:25 -0500

    First Commit.

---
+++ README.md
@@ -0,0 +1,9 @@
+# TweetSense
+An app that analyzes the emotion and tone of tweets. 
+
+## Authors 
+* **Vlad Munteanu** 
+
+## TO_DO 
+- [ ] Make App 
+
---
+++ TweetSense/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TweetSense
+//
+//  Created by Vlad Munteanu on 2/14/20.
+//  Copyright © 2020 Vlad Munteanu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
